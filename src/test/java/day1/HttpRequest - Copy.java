9c9689fb89a18738b75461f5ecad272ff39fab1c
SDETgit <shubhamjain007super@gmail.com>
2025-02-12 21:51:09 +0530

    coider 1

--- src/test/java/day1/HttpRequest - Copy.java
+++ src/test/java/day1/HttpRequest - Copy.java
@@ -104,6 +104,20 @@ int id;
 			.delete("https://reqres.in/api/users/"+id)
 		.then()
 			.statusCode(204);
+	}@Test(priority =5)
+	void deleteUser5(){
+		given().
+		when()
+			.delete("https://reqres.in/api/users/"+id)
+		.then()
+			.statusCode(204);
+	}@Test(priority =5)
+	void deleteUser5(){
+		given().
+		when()
+			.delete("https://reqres.in/api/users/"+id)
+		.then()
+			.statusCode(204);
 	}
 	}
 
